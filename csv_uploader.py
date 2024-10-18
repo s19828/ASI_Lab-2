216d711d758467fabf3c95e8eebe906f3e183b88
s19828 <s19828@pjwstk.edu.pl>
2024-10-18 20:57:19 +0200

    Update csv_uploader.py
    Added comments

--- csv_uploader.py
+++ csv_uploader.py
@@ -2,14 +2,18 @@ import gspread
 import os
 import pandas as pd
 
+# pobranie klucza API
 API_KEY = os.getenv('GOOGLE_API_KEY')
 SPREADSHEET_ID = '1c0q4djpUiU1xRmt4iOTKJkC87w7ZVU1XCPsXSUqcRTs'
 
+# uruchomienie clienta API
 gc = gspread.Client(None, API_KEY)
 
+# odczytanie danych pliku .csv
 df = pd.read_csv('PJATK-ASI-2024/Lab2---Obr-bka-danych/data_student_19828.csv')
 data = [df.columns.values.tolist()] + df.values.tolist()
 
+# wyczyszczenie arkusza i zapisanie zawartości z pliku .csv
 sh = gc.open_by_key(SPREADSHEET_ID)
 worksheet = sh.sheet1
 worksheet.clear()
